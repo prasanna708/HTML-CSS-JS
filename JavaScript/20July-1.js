67aabcdce7aeeb1ad7d078b17cec0cf5918ece24
prasannakumar4301 <prasannakumar4301@gmail.com>
2024-07-25 16:50:59 +0530

    v1

---
+++ JavaScript/20July-1.js
@@ -0,0 +1,15 @@
+const fun = () => {
+    document.write(`Hello! Good Morning...`);
+}
+
+const add = (x,y) => {
+    return x+y;
+}
+
+const product = (x,y) => x*y;
+
+const isEven = n => {
+    return n%2 === 0 ? 'Even' : 'Odd';
+}
+
+const nthsum = n => n * (n+1) / 2; 
